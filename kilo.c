e3d567d9b5e017b8f8c646a1704e7ec0cf22b565
tebii <105075744+tebii@users.noreply.github.com>
2024-08-05 21:12:42 +0800

    Add enable raw mode

--- kilo.c
+++ kilo.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return EXIT_SUCCESS;
