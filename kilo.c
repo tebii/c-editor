4ae363d24192f62f238fd445e7838173d20d92cb
tebii <105075744+tebii@users.noreply.github.com>
2024-08-07 23:27:04 +0800

    Add disabled misc flags

--- kilo.c
+++ kilo.c
@@ -13,8 +13,9 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ICRNL | IXON);
+  raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_lflag &= ~(OPOST);
+  raw.c_lflag |= ~(CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
