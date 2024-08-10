b103e458fc1e0b58dbab02baca69707cfd3184c8
tebii <105075744+tebii@users.noreply.github.com>
2024-08-10 13:25:29 +0800

    Add cursor movement

--- kilo.c
+++ kilo.c
@@ -15,6 +15,7 @@
 #define KILO_VERSION "0.0.1"
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -157,13 +158,32 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
 /*** input ***/
 
+void editorMoveCursor(char key) {
+  switch (key) {
+  case 'a':
+    E.cx--;
+    break;
+  case 'd':
+    E.cx++;
+    break;
+  case 'w':
+    E.cy--;
+    break;
+  case 's':
+    E.cy++;
+    break;
+  }
+}
 void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
@@ -172,12 +192,21 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+  case 'w':
+  case 's':
+  case 'a':
+  case 'd':
+    editorMoveCursor(c);
+    break;
   }
 }
 
 /*** init ***/
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
